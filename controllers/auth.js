f231d71b6ba4b3e0a30951158043f3e4f97cf4b2
Hau Tran <trandonhau1811@gmail.com>
2021-03-21 22:10:24 +0200

    Change the reset pwd path

--- controllers/auth.js
+++ controllers/auth.js
@@ -88,8 +88,8 @@ exports.forgotPassword = (req, res) => {
       from: "noreply@node-react.com",
       to: email,
       subject: "Password Reset Instructions",
-      text: `Please use the following link to reset your password: ${process.env.CLIENT_URL}/reset-password/${token}`,
-      html: `<p>Please use the following link to reset your password:</p> <p>${process.env.CLIENT_URL}/reset-password/${token}</p>`,
+      text: `Please use the following link to reset your password: http://143.198.76.152/reset-password/${token}`,
+      html: `<p>Please use the following link to reset your password:</p> <p>http://143.198.76.152/reset-password/${token}</p>`,
     };
 
     return user.updateOne({ resetPasswordLink: token }, (err, success) => {
